1ded2808e2a45149ad27909ed2753d21abc12955
Vitezslav Cupl <vitezslavcupl@gmail.com>
2026-04-11 18:52:02 +0200

    entity type def, labels/classes, supertypes

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,2 +1,31 @@
-class Evaluator:
-    raise NotImplementedError("Evaluator not implemented yet")
+from __future__ import annotations
+from typing import Dict, Set, Tuple
+
+Entity = Tuple[int, int, str] # (start, end, label)
+
+# CNEC entity classes
+CNEC_CLASSES: Set[str] = {
+    "A", "C", "P", "T",
+    "ah", "at", "az",
+    "g_", "gc", "gh", "gl", "gq", "gr", "gs", "gt", "gu",
+    "i_", "ia", "ic", "if", "io",
+    "me", "mi", "mn", "ms",
+    "n_", "na", "nb", "nc", "ni", "no", "ns",
+    "o_", "oa", "oe", "om", "op", "or",
+    "p_", "pc", "pd", "pf", "pm", "pp", "ps",
+    "td", "tf", "th", "tm", "ty",
+}
+
+# Label Studio aliases
+STUDIO_LABELS: Set[str] = {
+    "per", "loc_n", "loc_c", "loc_s", "ins", "med",
+    "obj_a", "obj_p", "tim", "evt", "ide", "groups", "misc", "amb",
+}
+
+# Supertype mapping for CNEC
+CNEC_SUPERTYPE: Dict[str, str] = {}
+for _tag in CNEC_CLASSES:
+    if len(_tag) == 1:
+        CNEC_SUPERTYPE[_tag] = _tag
+    else:
+        CNEC_SUPERTYPE[_tag] = _tag[0].upper()
